652adba5953bb36753cd808509c2a6515f0ac0fa
Adeem Adil Khatri <adil.khatri@ug.bilkent.edu.tr>
2022-11-05 01:17:12 +0300

    edit:  meals component added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import { Fragment } from "react";
 
 import mealsImg from "../../assets/meals.jpg";
 import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>MealsReact</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImg} alt="A table full of delicious food!"/>
