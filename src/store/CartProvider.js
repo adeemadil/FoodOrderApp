fd1e7ba7b769ea30875dc63275040b73740de150
Adeem Adil Khatri <adil.khatri@ug.bilkent.edu.tr>
2023-02-14 19:35:34 +0500

    add: fetching data from Firebase

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -15,7 +15,6 @@ const cartReducer = (state, action) => {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
-
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
 
@@ -35,7 +34,6 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
-
   if (action.type === "REMOVE") {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
